e24dcd125d9b0b0cdd28eaeb3a3cede48892f315
StellaKim1230 <stellakim1230@gmail.com>
2023-07-09 16:59:16 +0900

    WIP

---
+++ src/packages/design-styles/src/theme/types/CoComponentProps.ts
@@ -0,0 +1,8 @@
+import { CSSProperties } from 'react';
+
+export interface CoComponentProps<T extends string = never> {
+  className?: string;
+  style?: CSSProperties;
+  co?: Co;
+  overrideSTyles?: OverrideStyles<T>;
+}
